69e5c04c21d57589b5537cf859aa0d8a7cee206e
yacaFx <sergiobritor@gmail.com>
2018-12-17 05:27:24 -0500

    Create base components

---
+++ src/components/dish.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dish extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Platillo</h1>
+      </div>
+    );
+  }
+}
+
+export default Dish;
