84bdd0d9955946f73e42a3fa7c55051eb465f541
yacaFx <sergiobritor@gmail.com>
2018-12-17 05:44:48 -0500

    Using components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+
+import Header from "./components/header";
+import Dish from "./components/dish";
+
 import "./styles/App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hola Mundo 1</p>
+        <Header />
+        <Dish />
       </div>
     );
   }
