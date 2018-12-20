1975ae78073d7443c51596de79edb71d3a8eea54
yacaFx <sergiobritor@gmail.com>
2018-12-20 08:08:02 -0500

    Format code

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,10 @@
 import React, { Component } from "react";
 
 class Header extends Component {
-  //
-
-  /*
-
-
-  */
-
-  count() {
-    /*
-    const a = 3;
-    const b = 4;
-
-    return a * b;
-    */
-  }
-
   render() {
     return (
       <div>
-        <h4>Platillos Típicos</h4>
+        <h1>Platillos Típicos</h1>
       </div>
     );
   }
