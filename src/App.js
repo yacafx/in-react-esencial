5f0e98a1c64ffe83882328f3375e47d21c0ffa93
yacaFx <sergiobritor@gmail.com>
2018-12-20 08:30:27 -0500

    Add variables

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import Dish, { Flag, Ingredient } from "./components/dish";
 import "./styles/App.css";
 
 class App extends Component {
+  dish = "tacos";
+  dishes = ["Tacos", "Ceviche", "Paella"];
   render() {
     return (
       <div className="App">
         <Header />
         <Dish />
-        <Flag />
+        Yo como {this.dish}
+        <ul>
+          {this.dishes.map(dish => (
+            <li>{dish}</li>
+          ))}
+        </ul>
       </div>
     );
   }
