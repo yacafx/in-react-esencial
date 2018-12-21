66ab460e55d75e9c602c1e1abede99163ea9d38e
yacaFx <sergiobritor@gmail.com>
2018-12-21 09:04:16 -0500

    Add base events

---
+++ src/components/newDish.js
@@ -0,0 +1,33 @@
+import React, { Component, Fragment } from "react";
+import { Fab, TextField } from "@material-ui/core";
+
+import AddIcon from "@material-ui/icons/Add";
+
+class NewDish extends Component {
+  addDish() {
+    console.log("Agregar platillo");
+  }
+
+  render() {
+    return (
+      <form autoComplete="off">
+        <TextField
+          label="Platillo..."
+          type="text"
+          margin="normal"
+          variant="outlined"
+        />
+        <Fab
+          color="primary"
+          size="medium"
+          className="dish-form-icon"
+          onClick={this.addDish}
+        >
+          <AddIcon />
+        </Fab>
+      </form>
+    );
+  }
+}
+
+export default NewDish;
