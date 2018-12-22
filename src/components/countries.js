d24d313797773c0ae69afb6ebccc4a2632d2face
yacaFx <sergiobritor@gmail.com>
2018-12-22 08:03:20 -0500

    Add support files

---
+++ src/components/countries.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Countries extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Países</h1>
+      </div>
+    );
+  }
+}
+
+export default Countries;
---
+++ src/components/dishes.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Dishes extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Platillos</h1>
+      </div>
+    );
+  }
+}
+
+export default Dishes;
