9e14588e1b19f0b2356049c87880b531e82e2809
yacaFx <sergiobritor@gmail.com>
2018-12-21 19:19:55 -0500

    Cancel methods

--- src/components/newDish.js
+++ src/components/newDish.js
@@ -4,13 +4,15 @@ import { Fab, TextField } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 
 class NewDish extends Component {
-  addDish() {
+  addDish(event) {
     console.log("Agregar platillo");
+    event.preventDefault();
   }
 
   render() {
+    console.log(this);
     return (
-      <form autoComplete="off">
+      <form autoComplete="off" onSubmit={this.addDish}>
         <TextField
           label="Platillo..."
           type="text"
