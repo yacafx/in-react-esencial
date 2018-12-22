481082e94578e76c8d2ea31e1aae594d52a6ca89
yacaFx <sergiobritor@gmail.com>
2018-12-21 21:47:15 -0500

    Use ref values

--- src/components/newDish.js
+++ src/components/newDish.js
@@ -9,20 +9,26 @@ class NewDish extends Component {
   //   this.addDish = this.addDish.bind(this);
   // }
 
+  newDish = React.createRef();
+
   addDish = event => {
     console.log("Agregar platillo");
     event.preventDefault();
-    console.log(this);
+    // console.log(this.newDish.current.value);
+    console.log(this.newDish.value);
   };
 
   render() {
     return (
       <form autoComplete="off" onSubmit={this.addDish}>
+        {/* <input ref={this.newDish} /> */}
+
         <TextField
           label="Platillo..."
           type="text"
           margin="normal"
           variant="outlined"
+          inputRef={e => (this.newDish = e)}
         />
         <Fab
           color="primary"
