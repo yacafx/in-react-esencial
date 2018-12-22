a93aa8d8e8e95a28309f81153e1b3fadd249f03e
yacaFx <sergiobritor@gmail.com>
2018-12-22 08:28:37 -0500

    Use router

---
+++ src/components/router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "./../App";
+import Dishes from "./dishes";
+import Dish from "./dish";
+import Countries from "./countries";
+import NotFound from "./notFound";
+
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/platillos" component={Dishes} />
+      <Route path="/countries" component={Countries} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
