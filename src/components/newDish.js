957ba18865ac712c876f6c5db010dfca7e03540f
yacaFx <sergiobritor@gmail.com>
2018-12-21 19:39:23 -0500

    Bind methods

--- src/components/newDish.js
+++ src/components/newDish.js
@@ -4,13 +4,18 @@ import { Fab, TextField } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 
 class NewDish extends Component {
-  addDish(event) {
+  // constructor() {
+  //   super();
+  //   this.addDish = this.addDish.bind(this);
+  // }
+
+  addDish = event => {
     console.log("Agregar platillo");
     event.preventDefault();
-  }
+    console.log(this);
+  };
 
   render() {
-    console.log(this);
     return (
       <form autoComplete="off" onSubmit={this.addDish}>
         <TextField
