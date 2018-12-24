4c6c501a9c5e8a1dda4e042886a7c1e53cc08bde
yacaFx <sergiobritor@gmail.com>
2018-12-23 23:50:07 -0500

    Use Material for display dishes

--- src/components/dish.js
+++ src/components/dish.js
@@ -1,26 +1,14 @@
-import React, { Component, Fragment } from "react";
-import Button from "@material-ui/core/Button";
-
-export class Flag extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Bandera</h1>
-      </div>
-    );
-  }
-}
-
-export class Ingredient extends Component {
-  render() {
-    return (
-      <Fragment>
-        <h4>Ingredientes</h4>
-        <h4>Ingredientes</h4>
-      </Fragment>
-    );
-  }
-}
+import React, { Component } from "react";
+import {
+  Card,
+  CardContent,
+  List,
+  ListItem,
+  ListSubheader,
+  ListItemText,
+  ListItemIcon
+} from "@material-ui/core";
+import ScatterPlot from "@material-ui/icons/ScatterPlot";
 
 class Dish extends Component {
   ingredients = ["Tortilla", "Carne", "Cebolla"];
@@ -30,19 +18,26 @@ class Dish extends Component {
   }
 
   render() {
-    // const { params } = this.props.match;
     return (
-      <div className="dish">
-        {/* <h1>{params.name}</h1> */}
-        <h1>{this.props.name}</h1>
-        <h3>{this.countIngredients()}</h3>
-        <ul>
-          {this.ingredients.map((ingredient, index) => (
-            <li key={index}>{ingredient}</li>
-          ))}
-        </ul>
-        <Button variant="contained">Elegir</Button>
-      </div>
+      <Card className="card">
+        <CardContent>
+          <List
+            component="nav"
+            subheader={
+              <ListSubheader component="div">{this.props.name}</ListSubheader>
+            }
+          >
+            {this.props.ingredients.map((ingredient, index) => (
+              <ListItem button key={index}>
+                <ListItemIcon>
+                  <ScatterPlot />
+                </ListItemIcon>
+                <ListItemText inset primary={ingredient} />
+              </ListItem>
+            ))}
+          </List>
+        </CardContent>
+      </Card>
     );
   }
 }
