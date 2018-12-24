5a2caebafceea25f19454af4cd94b1e523a12c9e
yacaFx <sergiobritor@gmail.com>
2018-12-24 01:42:09 -0500

    Add remote data

--- src/components/countries.js
+++ src/components/countries.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
 
 export class Countries extends Component {
+  getCountries() {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div>
         <h1>Países</h1>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={this.getCountries}
+        >
+          Cargar lista
+        </Button>
       </div>
     );
   }
