d362b9828983eb8a45d878890af5940b0d9bcbfa
yacaFx <sergiobritor@gmail.com>
2018-12-31 03:08:29 -0500

    Delete component

--- src/components/countries.js
+++ src/components/countries.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
+import { Button } from "@material-ui/core";
+import { Country } from "./country";
 
 export class Countries extends Component {
   constructor() {
@@ -21,10 +22,30 @@ export class Countries extends Component {
       .catch(error => console.log(error));
   }
 
+  delete = name => {
+    let newState = { ...this.state };
+
+    newState.countries = newState.countries.filter(
+      country => country.name !== name
+    );
+
+    this.setState(newState);
+  };
+
+  componentWillUnmount() {
+    console.log("componente desmontado");
+  }
+
   render() {
     return (
       <div>
         <h1>Países</h1>
+        <ul>
+          {this.state.countries.map((country, index) => (
+            <Country key={index} name={country.name} onDelete={this.delete} />
+          ))}
+        </ul>
+
         <Button
           variant="contained"
           color="secondary"
